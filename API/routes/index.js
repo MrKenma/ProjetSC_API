473c59adbf99c9cf0b4ab7dad14ef54b09de6199
WangiWeberMDS <96393683+WangiWeberMDS@users.noreply.github.com>
2022-12-14 18:31:40 +0100

    Update index.js

--- API/routes/index.js
+++ API/routes/index.js
@@ -1,6 +1,10 @@
 const OrganizationRouter = require("./organization");
+const PartierRouter = require("./partier");
+const EventRouter = require("./event");
 const router = require("express").Router();
 
 router.use("/organization", OrganizationRouter);
+router.use("/partier", PartierRouter);
+router.use("/event", EventRouter);
 
 module.exports = router;
