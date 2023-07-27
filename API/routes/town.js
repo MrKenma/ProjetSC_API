e18146c956b5e71c1746e36fb67c690d01e62a2f
Simon Rollus <simrol260@gmail.com>
2023-07-27 20:35:33 +0200

    Update town.js

--- API/routes/town.js
+++ API/routes/town.js
@@ -5,7 +5,7 @@ const router = new Router;
 const IdentificationJWT = require('../middleware/IdentificationJWT');
 const Authorization = require('../middleware/Authorization');
 
-// Attention : si auth et id, ça va générer des erreurs dans l'app au moment du register
+// Attention : si auth et id, ça va générer des erreurs dans l'app au moment du register car pas de token à ce moment là
 router.get('/', TownController.getAllTowns);
 
 
