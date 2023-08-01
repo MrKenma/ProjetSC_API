15e4e35e87366e30046132e95b65b3f14b06e658
Simon_Rollus <simrol260@gmail.com>
2023-08-01 15:50:42 +0200

    Correction versioning

--- API/routes/index.js
+++ API/routes/index.js
@@ -8,13 +8,13 @@ const ImageRouter = require("./image");
 const UserRouter = require("./user");
 const router = require("express").Router();
 
-router.use("/1.0/organization", OrganizationRouter);
-router.use('/1.0/partier', PartierRouter);
-router.use('/1.0/shuttle', ShuttleRouter);
-router.use('/1.0/town', TownRouter);
-router.use('/1.0/event', EventRouter);
-router.use('/1.0/shuttleMember', ShuttleMemberRouter);
-router.use('/1.0/image', ImageRouter);
-router.use('/1.0/user', UserRouter);
+router.use("/1.0.0/organization", OrganizationRouter);
+router.use('/1.0.0/partier', PartierRouter);
+router.use('/1.0.0/shuttle', ShuttleRouter);
+router.use('/1.0.0/town', TownRouter);
+router.use('/1.0.0/event', EventRouter);
+router.use('/1.0.0/shuttleMember', ShuttleMemberRouter);
+router.use('/1.0.0/image', ImageRouter);
+router.use('/1.0.0/user', UserRouter);
 
 module.exports = router;
